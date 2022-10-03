7572a9118c004c8cd807e259599288366b659f9b
Łukasz Kużyński <lukasz.kuzynski@gmail.com>
2022-10-03 19:19:07 +0200

    Fixed type mistakes

--- packages/graphql/src/createQueryType.ts
+++ packages/graphql/src/createQueryType.ts
@@ -1,14 +1,13 @@
 import {InputTypeComposerFieldConfigMapDefinition} from "graphql-compose/lib/InputTypeComposer";
 import {ComposeInputType} from "graphql-compose/lib/utils/typeHelpers";
 import {InputTypeComposer} from "graphql-compose";
-import {GraphQLInputObjectType, GraphQLNonNull} from "graphql";
+import {GraphQLInputObjectType} from "graphql";
 
 export function createQueryType<TContext>({baseName, filtersType, paginationFields, sortType}: createQueryType.Options<TContext>) {
-	const wrappedFiltersType = filtersType instanceof GraphQLInputObjectType ? new GraphQLNonNull(filtersType) : filtersType.NonNull;
 	return InputTypeComposer.createTemp<TContext>({
 		name: `${baseName}_Query`,
 		fields: {
-			filters: {type: wrappedFiltersType},
+			filters: {type: filtersType},
 			...(sortType ? {
 				sortBy: {
 					type: sortType
--- packages/main/src/PaginableByOffset.ts
+++ packages/main/src/PaginableByOffset.ts
@@ -1,6 +1,11 @@
 export interface PaginableByOffset {
     offset?: number;
-    limit?: number;
+    /**
+     * Limit of entities to return
+     *
+     * This value is always provided due to default limit set in QueryBuilder
+     */
+    limit: number;
 }
 
 export namespace PaginableByOffset {
