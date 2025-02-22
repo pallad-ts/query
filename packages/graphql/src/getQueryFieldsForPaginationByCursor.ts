6575e399c34e49ee66baef0b147430ae99c5627f
Łukasz Kużyński <lukasz.kuzynski@gmail.com>
2025-02-22 08:28:24 +0100

    Fixed graphql types

--- packages/graphql/src/getQueryFieldsForPaginationByCursor.ts
+++ packages/graphql/src/getQueryFieldsForPaginationByCursor.ts
@@ -1,11 +1,11 @@
 import { GraphQLString } from "graphql";
-import { GraphQLPositiveInt } from "graphql-scalars";
+import { GraphQLNonNegativeInt } from "graphql-scalars";
 import { InputTypeComposerFieldConfigMapDefinition } from "graphql-compose/lib/InputTypeComposer";
 
 export function getQueryFieldsForPaginationByCursor(): InputTypeComposerFieldConfigMapDefinition {
 	return {
 		after: { type: GraphQLString },
 		before: { type: GraphQLString },
-		limit: { type: GraphQLPositiveInt },
+		limit: { type: GraphQLNonNegativeInt },
 	};
 }
